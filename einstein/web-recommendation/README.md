8b619c4a93c4ae240bc22476ed5caa6d586a0028
Jörn Berkefeld <joern.berkefeld@gmail.com>
2020-09-27 22:49:21 +0200

    einstein web recommendation: collect-code

---
+++ einstein/web-recommendation/README.md
@@ -0,0 +1,100 @@
+# Einstein Web-Recommendations
+
+This page aims to make using Einstein recommendations a little easier by adding a few explanations around how to use it in a modern setup.
+
+- [Einstein Web-Recommendations](#einstein-web-recommendations)
+	- [Collect Code](#collect-code)
+		- [Note to developers](#note-to-developers)
+		- [Collect Code on page load](#collect-code-on-page-load)
+		- [Asynchronous Collect Code](#asynchronous-collect-code)
+		- [Asynchronous Collect Code with preloading](#asynchronous-collect-code-with-preloading)
+
+## Collect Code
+
+> Official documentation: [help.salesforce.com](https://help.salesforce.com/articleView?id=mc_ctc_collect_code.htm&type=5)
+
+The default sample codes all assume a page load is happening whenever something needs to be tracked. However, the `collect.js` library is actually capable of being loaded asynchronously and can be used in a single-page-application (SPA) environment as well, similarly to [Google's analytics.js](https://developers.google.com/analytics/devguides/collection/analyticsjs).
+
+### Note to developers
+
+While the offical sample code snippets might lead you to believe that you are looking at an **Array**, that is in fact only half true. The variable `_etmc` gets rewritten by `collect.js` to become an object that happens to also expose a method named `push()`. Their base snippet in fact assumes that `collect.js` has been loaded synchronously before you execute your first call to `push()`. Essentially, the first Array-element is the name of an internal method of collect.js that gets executed.
+
+For more details, have a look at the [collect.js](collect-code/default/collect.js) library itself.
+
+### Collect Code on page load
+
+_Quoting from [SFMC's documentation](https://help.salesforce.com/articleView?id=mc_ctc_install_collect_code.htm&type=5):_
+
+The Collect Code should be placed just before the closing head tag and before any other Marketing Cloud code.
+
+To initialize the tracking, do not forget to replace the two occurences of "INSERT_MID" with your actual BU's Member ID.
+
+```html
+<script src='//INSERT_MID.collect.igodigital.com/collect.js'></script>
+```
+
+This code should be placed directly below the `</head>` tag following the above tag.
+
+```html
+<script>
+  _etmc.push(['setOrgId', 'INSERT_MID']);
+  _etmc.push(['trackPageView']);
+</script>
+```
+
+Download: [Default sample code](collect-code/default/collect-code.defaut.html)
+
+### Asynchronous Collect Code
+
+The Collect Code tag should be added near the top of the `<head>` tag and before any other script or CSS tags.
+
+To initialize the tracking, do not forget to replace the two occurences of "INSERT_MID" with your actual BU's Member ID.
+
+```html
+<!-- Einstein Collect Code -->
+<script>
+(function(e,t,c,n,o,s,a){e[o]=e[o]||[],s=t.createElement(c),a=t.getElementsByTagName(c)[0],s.async=1,s.src=n,a.parentNode.insertBefore(s,a)})(window,document,'script','//INSERT_MID.collect.igodigital.com/collect.js','_etmc');
+
+// always run this line once, followed by what you actually want to track; can be run programmatically in single-page-applications
+_etmc.push(['setOrgId', 'INSERT_MID']);
+
+// this can be run programmatically if your website is a single-page-application
+_etmc.push(['trackPageView']);
+</script>
+<!-- End Einstein Collect Code -->
+```
+
+Download: [Async sample code](collect-code/collect-code.async.html)
+
+The above code does four main things:
+
+1. Creates a `<script>` element that starts asynchronously downloading the collect.js JavaScript library from `https://INSERT_MID.collect.igodigital.com/collect.js`
+2. Initializes a global `_etmc` function that allows you to schedule commands to be run once the collect.js library is loaded and ready to go.
+3. Adds a command to the `_etmc` command queue to _set the Org ID_ to your Business Unit Member ID. This is required to ensure your tracking data ends up in the correct Business Unit.
+4. Adds another command to the `_etmc` command queue to track a [pageview](https://help.salesforce.com/articleView?id=mc_ctc_track_page_view.htm&type=5) to Einstein for the current page.
+
+Custom implementations may require modifying the last line of the above code snippet (the trackPageView command) or adding additional code to capture more interactions. However, you should not change the code that loads the collect.js library or initializes the `_etmc.push()` command queue function. Refer to the [official docs](https://help.salesforce.com/articleView?id=mc_ctc_collect_code.htm&type=5) for details on available options.
+
+### Asynchronous Collect Code with preloading
+
+The above Collect Code snippet will ensure things work across all browsers but it has the disadvantage of not allowing modern browsers to preload the script.
+
+The alternative async tag below adds support for preloading, which will provide a small performance boost on modern browsers, but can degrade to synchronous loading and execution on IE 9 and older mobile browsers that do not recognize the async script attribute. Only use this tag configuration if your visitors primarily use modern browsers to access your site.
+
+To initialize the tracking, do not forget to replace the two occurences of "INSERT_MID" with your actual BU's Member ID.
+
+```html
+<!-- Einstein Collect Code -->
+<script>
+window._etmc=window._etmc||[];
+
+_etmc.push(['setOrgId', 'INSERT_MID']);
+_etmc.push(['trackPageView']);
+</script>
+<script async src='//INSERT_MID.collect.igodigital.com/collect.js'></script>
+<!-- End Einstein Collect Code -->
+```
+
+Keep the above in the `<head>` section of your code.
+
+Download: [Async preload sample code](collect-code/collect-code.async-preload.html)
---
+++ einstein/web-recommendation/collect-code/default/collect.js
@@ -0,0 +1,310 @@
+/* eslint-env browser */
+/* eslint no-var:0, camelcase:0, no-redeclare:0, no-undef:0 */
+/* source: //MID.collect.igodigital.com/collect.js */
+
+if ('undefined' === typeof _etmc) {
+	var _etmc = [];
+}
+
+if (!Array.isArray) {
+	Array.isArray = function(arg) {
+		return Object.prototype.toString.call(arg) === '[object Array]';
+	};
+}
+
+var _etmc_temp = _etmc;
+var _etmc = {
+	debug: false,
+
+	setup: function(array) {
+		let l = array.length;
+		for (let i = 0; i < l; i++) {
+			this.callFunc(array[i]);
+		}
+	},
+
+	push: function(item) {
+		if (this.tracking_disabled) return;
+		this.callFunc(item);
+	},
+
+	callFunc: function(array) {
+		func_name = array[0];
+		args = array.slice(1, 3);
+		this[func_name](args);
+	},
+
+	setOrgId: function(args) {
+		if (Array.isArray(args[0]) === true) {
+			this.org_id = args[0];
+		} else {
+			this.org_id = [args[0]];
+		}
+	},
+
+	setUserInfo: function(args) {
+		let user_info = args[0];
+		if (this.user_info && this.user_info.email) {
+			user_info.email = this.user_info.email;
+		}
+		this.user_info = user_info;
+	},
+
+	setFirstParty: function(args) {
+		this.first_party = args[0];
+	},
+
+	setInsecure: function(args) {
+		this.insecure = args[0];
+	},
+
+	doNotTrack: function() {
+		this.tracking_disabled = true;
+	},
+
+	setConversionTrackingInfoFromUrl: function() {
+		this.email_job_id = this.getURLParameter('sfmc_j') || this.getURLParameter('j');
+		this.email = this.getURLParameter('sfmc_e') || this.getURLParameter('e');
+		if (this.email != null) {
+			if (this.user_info) {
+				this.user_info.email = this.email;
+			} else {
+				this.user_info = { email: this.email };
+			}
+		}
+		this.email_list_id = this.getURLParameter('sfmc_l') || this.getURLParameter('l');
+		this.email_landing_url_id = this.getURLParameter('sfmc_u') || this.getURLParameter('u');
+		this.email_job_batch_id = this.getURLParameter('sfmc_jb') || this.getURLParameter('jb');
+		this.email_subscriber_id =
+			this.getURLParameter('sfmc_s') || this.getURLParameter('s') || this.getURLParameter('sfmc_sub');
+		this.mid = this.getURLParameter('sfmc_mid') || this.getURLParameter('mid');
+	},
+
+	isEtConversionTracking: function() {
+		return (
+			this.email_job_id != null &&
+			(this.email != null || this.email_subscriber_id != null) &&
+			this.email_list_id != null &&
+			this.email_landing_url_id != null &&
+			this.email_job_batch_id != null &&
+			this.mid != null
+		);
+	},
+
+	getCurrentLocation: function() {
+		return window.location.href;
+	},
+
+	trackPageView: function(args) {
+		this.setConversionTrackingInfoFromUrl();
+		let pageView = args[0] || {};
+		if (this.org_id) {
+			pageView.title = pageView.title || document.title;
+			pageView.url = pageView.url || this.getCurrentLocation();
+			pageView.referrer = document.referrer;
+			if (this.isEtConversionTracking()) {
+				pageView.et_email = {};
+				pageView.et_email.job_id = this.email_job_id;
+				pageView.et_email.list_id = this.email_list_id.replace(/\D+/, '');
+				pageView.et_email.landing_url_id = this.email_landing_url_id;
+				pageView.et_email.job_batch_id = this.email_job_batch_id;
+				pageView.et_email.subscriber_id = this.email_subscriber_id;
+				pageView.et_email.mid = this.mid;
+			}
+			this.sendDataWithImage('track_page_view', pageView);
+		}
+	},
+
+	trackCart: function(args) {
+		let cart = args[0] || {};
+		if (this.org_id) {
+			let cartInfo = this.standardizeCart(cart);
+			if ('clear_cart' in cart) {
+				cartInfo.clear_cart = cart.clear_cart;
+			}
+			cartInfo.url = this.getCurrentLocation();
+			this.sendDataWithImage('track_cart', cartInfo);
+		}
+	},
+
+	trackConversion: function(args) {
+		let cart = args[0] || [];
+		let opts = args[1];
+		if (this.org_id) {
+			let cartInfo = this.standardizeCart(cart);
+			if (cart['details']) {
+				cartInfo.details = cart['details'];
+			} else if (opts) {
+				if ('details' in opts) {
+					cartInfo.details = opts.details;
+				}
+				if ('currency_code' in opts) {
+					cartInfo.currency_code = opts.currency_code;
+				}
+			}
+			cartInfo.url = this.getCurrentLocation();
+			this.sendDataWithImage('track_conversion', cartInfo);
+		}
+	},
+
+	trackEvent: function(args) {
+		let customEvent = args[0] || {};
+		let customName = customEvent.name;
+		let newEvent = {};
+		if (this.org_id) {
+			if (customName !== null) {
+				newEvent.event_name = customName;
+				if (customEvent.details) {
+					newEvent.details = customEvent.details;
+				}
+				this.sendDataWithImage('track_event', newEvent);
+			}
+		}
+	},
+
+	trackWishlist: function(args) {
+		let wishlist = args[0] || [];
+		if (this.org_id) {
+			if (wishlist != []) {
+				this.sendDataWithImage('track_wishlist', wishlist);
+			}
+		}
+	},
+
+	trackRating: function(args) {
+		let rating = args[0] || {};
+		if (this.org_id) {
+			if (rating.rating != null) {
+				this.sendDataWithImage('track_rating', rating);
+			}
+		}
+	},
+
+	sendDataWithImage: function(endpoint, args) {
+		if (this.user_info) {
+			args.user_info = this.user_info;
+		}
+		if (this.org_id.length > 1) {
+			temp_org_id = [this.org_id.shift()];
+			args.child_ids = this.org_id;
+			this.org_id = temp_org_id;
+		}
+
+		let collectData = encodeURIComponent(JSON.stringify(args));
+		let protocolPrepend = 'https';
+		if (this.insecure === true && document.location.protocol === 'http:') protocolPrepend = 'http';
+		if (this.first_party) {
+			collect_url = this.first_party;
+		} else {
+			collect_url = 'tau.collect.igodigital.com';
+		}
+
+		let image_src =
+			protocolPrepend + '://' + collect_url + '/c2/' + this.org_id + '/' + endpoint + '?payload=' + collectData;
+		let image_tag = document.createElement('img');
+		image_tag.src = image_src;
+		image_tag.style.display = 'none';
+		image_tag.width = '0';
+		image_tag.height = '0';
+		image_tag.title = 'blank image';
+		image_tag.setAttribute('aria-hidden', 'true');
+
+		if (this.debug) {
+			console.log(args);
+			console.log(image_tag.outerHTML);
+		} else {
+			let etmc_element = document.createElement('div');
+			etmc_element.style.display = 'none';
+			etmc_element.innerHTML = image_tag.outerHTML;
+			document.body.appendChild(etmc_element);
+		}
+	},
+
+	standardizeCart: function(cart) {
+		let cartInfo = {};
+		if (Array.isArray(cart) === true) {
+			cartInfo = { cart: cart };
+		} else if ('cart' in cart) {
+			cartInfo = cart;
+		} else if ('item' in cart) {
+			cartInfo = { cart: [cart] };
+		}
+		return cartInfo;
+	},
+
+	updateItem: function(args) {
+		let rec_items = args[0];
+		if (rec_items !== null) {
+			let items_to_update = null;
+
+			if (!Array.isArray(rec_items)) {
+				rec_items = [rec_items];
+			}
+
+			let valid_items = [];
+
+			for (let i in rec_items) {
+				if (rec_items.hasOwnProperty(i)) {
+					this.setDefaultItemValues(rec_items[i]);
+					if (this.hasRequiredFields(rec_items[i])) {
+						valid_items.push(rec_items[i]);
+					} else {
+						console.log(
+							'ETMC update_rec_item error: required fields were missing from ' +
+								JSON.stringify(rec_items[i])
+						);
+					}
+				}
+			}
+
+			if (valid_items.length > 0) {
+				items_to_update = valid_items;
+			}
+
+			if (items_to_update !== null) {
+				this.sendDataWithImage('update_item', items_to_update);
+			}
+		}
+	},
+
+	getURLParameter: function(paramName) {
+		let regex = new RegExp('[?|&]' + paramName + '=' + '([^&;]+?)(&|#|;|$)');
+		let matches = regex.exec(window.location.search) || [, ''];
+		let parameterValue = matches[1].replace(/\+/g, '%20');
+		return decodeURIComponent(parameterValue) || null;
+	},
+
+	hasRequiredFields: function(item) {
+		let isValid = item['item'] !== undefined && item['url'] !== undefined;
+
+		if (isValid) {
+			let item_type = item['item_type'];
+			if (this.shouldValidateName(item_type)) {
+				isValid = item['name'] != undefined;
+			} else if (item_type === 'banner') {
+				isValid =
+					item['image_url'] !== undefined &&
+					(item['banner_size'] !== undefined || item['location_size'] !== undefined);
+			}
+		}
+		return isValid;
+	},
+
+	shouldValidateName: function(item_type) {
+		return item_type == undefined || item_type === 'product' || item_type === 'content' || item_type === 'asset';
+	},
+
+	setDefaultItemValues: function(item) {
+		if (item['item_type'] == undefined) {
+			item['item_type'] = 'content';
+		}
+		if (item['url'] == undefined) {
+			item['url'] = window.location.href;
+		}
+		if (item['item_type'] !== 'banner' && item['item_type'] !== 'asset' && item['name'] == undefined) {
+			item['name'] = document.title;
+		}
+	}
+};
+
+_etmc.setup(_etmc_temp);
